e474572b282ed4a17d6a49be2364b891cdb9b8cd
Sumit Agrawal <45976295+sumit-agr@users.noreply.github.com>
2020-05-27 11:44:39 +0530

    Update diseaseprediction.py

--- diseaseprediction.py
+++ diseaseprediction.py
@@ -9,19 +9,19 @@ cols = df.columns
 cols = cols[:-1]
 x = df[cols]
 y = df['prognosis']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=44)
 
 print ("DecisionTree")
 dt = DecisionTreeClassifier()
 clf_dt=dt.fit(x_train,y_train)
-indices = [i for i in range(132)]
+indices = [i for i in range(133)]
 symptoms = df.columns.values[:-1]
 
 dictionary = dict(zip(symptoms,indices))
 
 def dosomething(symptom):
     user_input_symptoms = symptom
-    user_input_label = [0 for i in range(132)]
+    user_input_label = [0 for i in range(133)]
     for i in user_input_symptoms:
         idx = dictionary[i]
         user_input_label[idx] = 1
